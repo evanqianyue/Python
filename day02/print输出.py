46b962e429196437e708a32800065debf51498d4
evanqianyue <evanqianyue@163.com>
2018-10-27 15:47:02 +0800

    test

---
+++ day02/print输出.py
@@ -0,0 +1,14 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+ @ Author     ：Evan
+ @ Date       ：2018/10/27 15:35
+ @ Version    : 1.0
+ @ Description：
+ @ Modified By：
+"""
+
+# 可以接受多个字符串，用逗号分割，遇到逗号会输出一个空格
+print("hello", "name", "age")
+print("10 + 9 =", 19)
---
+++ day02/注释.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+ @ Author     ：Evan
+ @ Date       ：2018/10/27 15:32
+ @ Version    : 1.0
+ @ Description：
+ @ Modified By：
+"""
+
+"""
+这些是注释
+"""
+
+print("hello123")
